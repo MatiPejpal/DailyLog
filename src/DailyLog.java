0faf724c8e52802dd9a6a570bef5803f82367cbe
MatiPejpal <matipejpal@gmail.com>
2022-02-01 13:58:56 +0100

    Creates EntryWriter class, which is responsible for creating new entries. Sets up interface for this class.

--- src/DailyLog.java
+++ src/DailyLog.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import java.awt.*;
 import java.util.Date;
 
@@ -7,11 +8,12 @@ class DailyLog extends JFrame {
 
     DailyLog(){
         init(this);
+        add(new EntryWriter());
+        pack();
     }
 
-    static void init(JFrame f){
+    static private void init(JFrame f){
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(new Dimension(400, 800));
         f.getContentPane().setBackground(Color.gray);
         f.setLocationRelativeTo(null);
         f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
@@ -19,6 +21,68 @@ class DailyLog extends JFrame {
     }
 
     public static void main(String[] args){ SwingUtilities.invokeLater(DailyLog::new); }
+
+}
+
+class EntryWriter extends JPanel{
+    // Class that allow user to write and save entries.
+
+    EntryWriter(){
+        init(this);
+        final JTextField dateField = createDateField();
+        final JTextArea activityField = createActivityField();
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        add(dateField, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        add(addButton(), c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        add(activityField, c);
+    }
+
+    static private void init(JPanel p){
+        p.setBackground(Color.gray);
+        p.setLayout(new GridBagLayout());
+    }
+
+    static private JTextField createDateField(){
+        JTextField t = new JTextField();
+        t.setBackground(Color.gray);
+        t.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createLineBorder(Color.black, 1)));
+        t.setPreferredSize(new Dimension(360, 40));
+        t.setMaximumSize(new Dimension(360, 40));
+        t.setVisible(true);
+        return t;
+    }
+
+    static private JTextArea createActivityField(){
+        JTextArea t = new JTextArea();
+        t.setBackground(Color.gray);
+        t.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createLineBorder(Color.black, 1)));
+        t.setPreferredSize(new Dimension(400, 100));
+        t.setMinimumSize(new Dimension(360, 40));
+        t.setVisible(true);
+        return t;
+    }
+
+    static private JButton addButton(){
+        JButton b = new JButton("+");
+        b.setPreferredSize(new Dimension(40,40));
+        b.setMaximumSize(new Dimension(40,40));
+        b.setFocusPainted(false);
+        b.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createSoftBevelBorder(0, Color.gray, Color.darkGray)));
+        b.setBackground(Color.gray);
+        b.setVisible(true);
+        return b;
+    }
 
 }
 
