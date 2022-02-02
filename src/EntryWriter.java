83d3b03f6c3d1b3943491866192148735bc992b7
MatiPejpal <matipejpal@gmail.com>
2022-02-02 23:32:40 +0100

    Separates DailyLog and EntryWriter into different files.

---
+++ src/EntryWriter.java
@@ -0,0 +1,93 @@
+import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+class EntryWriter extends JPanel {
+    // Class that allow user to write and save entries.
+
+    EntryWriter(){
+        init(this);
+        final GhostTextField dateField = createDateField();
+        final JTextArea activityField = createActivityField();
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        add(dateField, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        add(createAddButton(), c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 2;
+        add(activityField, c);
+    }
+
+    static private void init(JPanel p){
+        p.setBackground(Color.gray);
+        p.setLayout(new GridBagLayout());
+    }
+
+    static private GhostTextField createDateField(){
+        // creates text field for date
+        GhostTextField t = new GhostTextField("DD.MM.YYYY");
+        t.setBackground(Color.gray);
+        t.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createLineBorder(Color.black, 1)));
+        t.setPreferredSize(new Dimension(360, 40));
+        t.setMaximumSize(new Dimension(360, 40));
+        t.setVisible(true);
+        return t;
+    }
+
+    static private JTextArea createActivityField(){
+        // creates text area for substance of entries (activities)
+        JTextArea t = new JTextArea();
+        t.setBackground(Color.gray);
+        t.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createLineBorder(Color.black, 1)));
+        t.setPreferredSize(new Dimension(400, 100));
+        t.setMinimumSize(new Dimension(360, 40));
+        t.setVisible(true);
+        return t;
+    }
+
+    static private JButton createAddButton(){
+        // creates button that will save and add entry.
+        JButton b = new JButton("+");
+        b.setPreferredSize(new Dimension(40,40));
+        b.setMaximumSize(new Dimension(40,40));
+        b.setFocusPainted(false);
+        b.setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(1,1,1,1), BorderFactory.createSoftBevelBorder(0, Color.gray, Color.darkGray)));
+        b.setBackground(Color.gray);
+        b.setVisible(true);
+        return b;
+    }
+
+    private static class GhostTextField extends JTextField{
+        // class that modifies JTextField to add to it ghost text.
+        GhostTextField(final String ghostText){
+            super(ghostText);
+            addFocusListener(new FocusListener() {
+
+                @Override
+                public void focusLost(FocusEvent e) {
+                    if(getText().isEmpty()) {
+                        setText(ghostText);
+                    }
+                }
+
+                @Override
+                public void focusGained(FocusEvent e) {
+                    if(getText().equals(ghostText)) {
+                        setText("");
+                    }
+                }
+            });
+        }
+    }
+
+}
